33047a054177bef6ca1c4b7fdaae12c928ea92ef
rodrigo <kalvans.rodrigo@gmail.com>
2023-08-28 14:41:19 +0200

    fix: tcorvus url

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,5 +1,6 @@
-export const TCORVUS = "https://main.d12aaacbrok4tp.amplifyapp.com/";
+export const TCORVUS = "https://tcorvus.vercel.app/";
 export const PORTFOLIO = "https://www.rodrigok.page/";
+
 export const GITHUB = "https://github.com/RodrigoKalvans";
 export const LINKEDIN = "https://www.linkedin.com/in/rodrigo-kalvans/";
 export const EMAIL = "kalvans.rodrigo@gmail.com";
